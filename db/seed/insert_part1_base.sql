d87e6950d296efaa283313ca3e75dde6dca84f42
s6604053630405-sirasak <s6604053630405@email.kmutnb.ac.th>
2026-07-15 10:42:32 +0700

    change structure insert

--- db/seed/insert_part1_base.sql
+++ db/seed/insert_part1_base.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2049c32b2156a15b1028ebe4a0190ad0e006c72405197c39a1483af2ef04fd2
-size 111680
+oid sha256:8d1a803ee73d2a3adb699aeeb0f16d6a7833996d9ab518f72adf935350be69b4
+size 88765
