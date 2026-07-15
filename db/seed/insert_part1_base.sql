bf43a314ab881669cafb066a0a34d2f0f96d0ad8
s6604053630405-sirasak <s6604053630405@email.kmutnb.ac.th>
2026-07-15 13:16:22 +0700

    add staff_credential insert_2

--- db/seed/insert_part1_base.sql
+++ db/seed/insert_part1_base.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1a803ee73d2a3adb699aeeb0f16d6a7833996d9ab518f72adf935350be69b4
-size 88765
+oid sha256:ab00583d4f805971a1149cea8f03df27a40028243e22aeac2598ee7e38b36ea5
+size 79936
--- db/seed/insert_part2_summary.sql
+++ db/seed/insert_part2_summary.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2555ffe7ffcbe3ed7015b7cd35fbb802727f40f0bfba510c9ce113ee71a20470
-size 37135
+oid sha256:ea50b69c05f3463a74ef6cf6d77f3c0281d978ba115f8ecadb7daddbe0072f8b
+size 45964
