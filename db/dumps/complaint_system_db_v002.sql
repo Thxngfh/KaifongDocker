c1fcc6aec300c2fdacf559cdf9acebd4593df884
s6604053630405-sirasak <s6604053630405@email.kmutnb.ac.th>
2026-07-15 14:58:57 +0700

    Reverse Version & Drop AI Table

--- db/dumps/complaint_system_db_v002.sql
+++ db/dumps/complaint_system_db_v002.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2eb2d077d56d8c68d79e0156e30cafbd428b059a3cd80d07c427e4f4f488498
-size 114348582
+oid sha256:7ea37c224fabe9d13257758a6fe8705f561a53706e269e6a0edd78da63d0d1f5
+size 114348789
