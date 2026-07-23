16e3d67738a33f290a636340f4dd20a6d4bd52bd
s6604053630405-sirasak <s6604053630405@email.kmutnb.ac.th>
2026-07-23 12:41:41 +0700

    edit migration database

--- db/init/create-table.sql
+++ db/init/create-table.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:244205d78073c98dff760afe020fe9bf9ee719eedf8a20e9e202a16b08f7ed71
-size 45638
+oid sha256:42ea9e3544036d35fe898805ee98929f6a59b31f67228cae103036096602617d
+size 45755
